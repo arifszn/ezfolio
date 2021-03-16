5e67e840150639907cd7a95cc2a80733435e8ee6
Ariful Alam <arif@orangetoolz.com>
2021-03-16 20:09:09 +0600

    Add helper files

---
+++ app/Helpers/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+class Constants {
+    const TRUE = 1;
+    const FALSE = 0;
+
+    //response code (More at: vendor\symfony\http-foundation\Response.php)
+    const STATUS_CODE_SUCCESS = 200;
+    const STATUS_CODE_BAD_REQUEST = 400;
+    const STATUS_CODE_UNAUTHORIZED = 401;
+    const STATUS_CODE_NOT_FOUND = 404;
+    const STATUS_CODE_ERROR = 500;
+}
