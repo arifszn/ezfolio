d7ec21f2976659cbf233ac7a8caf1fb9953d3bf8
Ariful Alam <arif@orangetoolz.com>
2021-04-07 01:15:17 +0600

    Center spinner

--- resources/js/client/common/components/lazyLoadingFallbackUi/LazyLoadingFallbackUi.js
+++ resources/js/client/common/components/lazyLoadingFallbackUi/LazyLoadingFallbackUi.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`
     display: flex;
+    position: absolute;
     justify-content: center;
     align-items: center;
     width: 100%;
