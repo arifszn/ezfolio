e4d26a89bd6a77801724f130c475dbfe9ec5fbf3
Ariful Alam <arif@orangetoolz.com>
2021-04-09 12:19:19 +0600

    Customize skeleton

--- resources/js/client/admin/components/layout/PageWrapper.js
+++ resources/js/client/admin/components/layout/PageWrapper.js
@@ -2,11 +2,11 @@ import { Card, Skeleton } from "antd"
 import React from "react";
 import PropTypes from 'prop-types';
 
-const PageWrapper = ({children, className = '', loading = false, loadingRows = 8}) => {
+const PageWrapper = ({children, className = '', loading = false, loadingRows = 10}) => {
     return (
         <React.Fragment>
             <Card bordered={false} bodyStyle={loading ? {} : {padding: 0}} hoverable className={`${className} z-shadow`} >
-                <Skeleton loading={loading} active paragraph={{ rows: loadingRows }}>
+                <Skeleton loading={loading} title={false} active paragraph={{ rows: loadingRows, width: '100%' }}>
                     {children}
                 </Skeleton>
             </Card>
